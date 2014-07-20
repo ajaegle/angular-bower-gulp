df1c72984dc24853c8796827bc4449b550fea655
ajaegle <mail@ajaegle.de>
2014-07-20 22:22:26 +0200

    add another gulp task that has some more steps, using gulp-concat and gulp-jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp'),
-   uglify = require('gulp-uglify');
+   uglify = require('gulp-uglify'),
+   jshint = require('gulp-jshint'),
+   concat = require('gulp-concat');
 
 gulp.task('minify', function () {
    gulp.src('js/app.js')
       .pipe(uglify())
       .pipe(gulp.dest('build'))
+});
+
+
+gulp.task('js', function () {
+   return gulp.src('js/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'))
+      .pipe(uglify())
+      .pipe(concat('app.js'))
+      .pipe(gulp.dest('build'));
 });
