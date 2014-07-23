f06e5f6d3d9be49c597c9af1b107e9207d149065
ajaegle <mail@ajaegle.de>
2014-07-24 00:46:59 +0200

    use gulp-notify to show mac os notifications

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
    uglify = require('gulp-uglify'),
    jshint = require('gulp-jshint'),
    concat = require('gulp-concat');
+var notify = require('gulp-notify');
 
 gulp.task('minify', function () {
    gulp.src('js/app.js')
@@ -16,5 +17,6 @@ gulp.task('js', function () {
       .pipe(jshint.reporter('jshint-stylish'))
       .pipe(uglify())
       .pipe(concat('app.js'))
-      .pipe(gulp.dest('build'));
+      .pipe(gulp.dest('build'))
+	  .pipe(notify('js task completed.'));
 });
