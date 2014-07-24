77aa6ea16fcbe85cacfa0fd7807d8d0eaa399b9c
ajaegle <mail@ajaegle.de>
2014-07-24 21:57:38 +0200

    use gulp-webserver for live reloading
    currently still needs manual trigger for html and js tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
    jshint = require('gulp-jshint'),
    concat = require('gulp-concat');
 var notify = require('gulp-notify');
+var webserver = require('gulp-webserver');
 
 gulp.task('minify', function () {
    gulp.src('app/js/app.js')
@@ -17,6 +18,18 @@ gulp.task('js', function () {
       .pipe(jshint.reporter('jshint-stylish'))
       .pipe(uglify())
       .pipe(concat('app.js'))
-      .pipe(gulp.dest('build'));
+      .pipe(gulp.dest('build/js/'));
 	    // .pipe(notify('js task completed.'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('app/**/*.html')
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('webserver', function() {
+  gulp.src('build')
+    .pipe(webserver({
+      livereload: true
+    }));
 });
