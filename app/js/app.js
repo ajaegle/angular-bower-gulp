4d643a3571caf9eb8490d361a88530feec68a623
ajaegle <mail@ajaegle.de>
2014-09-08 01:33:02 +0200

    add first angular module and controller

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,9 @@
 (function () {
   $('div.content').html('<h1>new content</h1>');
+
+  angular.module('aj', []);
+  angular.module('aj')
+    .controller('MainCtrl', ['$scope', function ($scope) {
+      $scope.name = 'ajaegle';
+    }]);
 })();
