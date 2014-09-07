0d091b5675aafb98b8c957ba1ce6a9a5ee46a3a4
ajaegle <mail@ajaegle.de>
2014-09-08 01:40:51 +0200

    add rectangular and sample call to github api

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,24 @@
 (function () {
   $('div.content').html('<h1>new content</h1>');
 
-  angular.module('aj', []);
+  angular.module('aj', ['restangular']);
   angular.module('aj')
-    .controller('MainCtrl', ['$scope', function ($scope) {
-      $scope.name = 'ajaegle';
-    }]);
+    .controller('MainCtrl', ['$scope', 'Restangular',
+      function ($scope, Restangular) {
+        $scope.name = 'ajaegle';
+        $scope.user = {};
+
+        var User = Restangular.allUrl('users', 'https://api.github.com/users');
+        $scope.fetchUser = function (name) {
+          User.one(name).get().then(
+            function (data) {
+              $scope.user = data;
+            },
+            function (err) {
+              $scope.err = err;
+            }
+          )
+        };
+        $scope.fetchUser($scope.name);
+      }]);
 })();
