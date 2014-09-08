4869358ce9685a992a9a6f596f42f99c5d9e9d3f
ajaegle <mail@ajaegle.de>
2014-09-08 23:57:17 +0200

    move controller into own file, referencing the existing ng module

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,3 @@
 (function () {
   angular.module('aj', ['restangular']);
-  angular.module('aj')
-    .controller('MainCtrl', ['$scope', 'Restangular',
-      function ($scope, Restangular) {
-        $scope.name = 'ajaegle';
-        $scope.user = {};
-
-        var User = Restangular.allUrl('users', 'https://api.github.com/users');
-        $scope.fetchUser = function (name) {
-          User.one(name).get().then(
-            function (data) {
-              $scope.user = data;
-              console.log($scope.user.login);
-            },
-            function (err) {
-              $scope.err = err;
-            }
-          );
-        };
-        $scope.fetchUser($scope.name);
-      }]);
 })();
