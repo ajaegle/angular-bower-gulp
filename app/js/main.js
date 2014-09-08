38f34299d5c290a3c04201bc63ebe5fb37879773
ajaegle <mail@ajaegle.de>
2014-09-09 00:26:49 +0200

    split definition of controller and its registration to separate parts.

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,25 @@
 (function () {
-  angular.module('aj')
-    .controller('MainCtrl', ['$scope', 'Restangular',
-      function ($scope, Restangular) {
-        $scope.name = 'ajaegle';
-        $scope.user = {};
 
-        var User = Restangular.allUrl('users', 'https://api.github.com/users');
-        $scope.fetchUser = function (name) {
-          User.one(name).get().then(
-            function (data) {
-              $scope.user = data;
-              console.log($scope.user.login);
-            },
-            function (err) {
-              $scope.err = err;
-            }
-          );
-        };
-        $scope.fetchUser($scope.name);
-      }]);
+  function MainCtrl ($scope, Restangular) {
+    $scope.name = 'ajaegle';
+    $scope.user = {};
+
+    var User = Restangular.allUrl('users', 'https://api.github.com/users');
+    $scope.fetchUser = function (name) {
+      User.one(name).get().then(
+        function (data) {
+          $scope.user = data;
+          console.log($scope.user.login);
+        },
+        function (err) {
+          $scope.err = err;
+        }
+      );
+    };
+    $scope.fetchUser($scope.name);
+  }
+
+  angular.module('aj')
+    .controller('MainCtrl', ['$scope', 'Restangular', MainCtrl]);
+
 }) ();
