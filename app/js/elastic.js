b8bfdd008254d1608c4e9c73cab6ec0db0cf5524
ajaegle <mail@ajaegle.de>
2014-09-09 23:58:01 +0200

    Add controller for accessing elasticsearch instance
    Currently using hard coded urls to elasticsearch. this should be refactored to a provider to be able to run a config at startup.
    Elasticsearch can be used easily with a vagrant instance from e.g. https://github.com/ajaegle/ansible-elasticsearch

---
+++ app/js/elastic.js
@@ -0,0 +1,57 @@
+(function () {
+  'use strict';
+
+  /* @ngInject */
+  function ElasticService ($http) {
+    var server = "http://192.168.111.10:9200/";
+    var path = "weather/record/_search";
+
+    var query =
+      {
+        "query": {
+          "filtered": {
+            "filter": {
+              "bool": {
+                "must": [
+                  {"term": {
+                    "stationsName": "Karlsruhe"
+                  }}
+                ]
+              }
+            }
+          }
+        }
+      };
+
+    this.readData = function (name) {
+      var url = server + path;
+      return $http.post(url, query);
+    };
+  }
+
+
+  /* @ngInject */
+  function ElasticCtrl (ElasticService) {
+    var vm = this;
+    this.read = function () {
+      ElasticService.readData().then(
+        function (data) {
+          vm.result = data;
+          vm.error = false;
+          console.log(data);
+        },
+        function (data) {
+          vm.result = data;
+          vm.error = true;
+          console.log(data);
+        }
+      );
+    };
+    this.read();
+  }
+
+  angular.module('aj')
+    .service('ElasticService', ElasticService)
+    .controller('ElasticCtrl', ElasticCtrl);
+
+}) ();
