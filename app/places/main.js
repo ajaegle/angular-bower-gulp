1a99f2af122f18e2334cdf4a4fb2347ea1c86f58
ajaegle <mail@ajaegle.de>
2014-09-16 13:48:43 +0200

    center leaflet to user's position and show some markers

--- app/places/main.js
+++ app/places/main.js
@@ -5,13 +5,45 @@
   function PlacesService ($http) {
   }
 
+  var places = {
+    castle: {
+      lat: 49.013604,
+      lng: 8.404419
+    },
+    work: {
+      lat: 49.020498,
+      lng: 8.437560
+    }
+  };
 
   /* @ngInject */
-  function PlacesCtrl (PlacesService) {
-    this.items = ["a", "b", "c"];
+  function PlacesCtrl (Places) {
+    var vm = this;
+    vm.markers = {};
+
+    vm.center = {
+      lat: 49.009356,
+      lng: 8.403697,
+      zoom: 16,
+      autoDiscover: true
+    };
+
+    vm.addMarkers = function() {
+      vm.markers.work = Places.work;
+      vm.markers.castle = Places.castle;
+    };
+
+    vm.removeMarkers = function() {
+      vm.markers = {};
+    };
+
+    vm.locateMe = function () {
+      vm.center.autoDiscover = true;
+    };
   }
 
   angular.module('aj')
+    .value('Places', places)
     .service('PlacesService', PlacesService)
     .controller('PlacesCtrl', PlacesCtrl);
 
